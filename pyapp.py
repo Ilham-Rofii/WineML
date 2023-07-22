0bf79d4b1045f9e43a0b02cda1dd2369c87fae7e
Ilham-Rofii <95387119+Ilham-Rofii@users.noreply.github.com>
2023-07-22 22:26:24 +0700

    Update pyapp.py

--- pyapp.py
+++ pyapp.py
@@ -26,7 +26,7 @@ rf_classif = load("rf_classif.model")
 Y_test_preds = rf_classif.predict(X_test)
 
 ## Dashboard
-st.title("Wine Type :red[Prediction]")
+st.title("       Wine Type :red[Prediction]")
 st.markdown("Predict Wine Type using Ingredients Values :red[By Ilham Rofi'i]")
 
 tab1, tab2, tab3 = st.tabs(["Data :clipboard:", "Global Performance :weight_lifter:", "Local Performance :bicyclist:"])
